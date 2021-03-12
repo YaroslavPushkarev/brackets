b314847cd0e6ef4773932aa24c8a746ce8913254
Your Name <yarik2015@protonmail.com>
2021-03-12 14:22:35 +0200

    feat: solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strConfig = bracketsConfig.map(item => item.join(''));
+  
+  for (let i = 0; i < strConfig.length; i++) {
+    if (str.includes(strConfig[i])) {
+      str = str.replace(strConfig[i], '');
+      i = -1;
+    }
+  }
+   if(str.length === 0){ 
+     return true
+    }else {
+      return false
+    };
 }
